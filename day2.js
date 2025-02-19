882f2a70b256c2aaacc6a638d8dd762557d7c7ed
Abhikatharshala <katharashalaabhilash421@gmail.com>
2025-02-20 00:00:59 +0530

    msg

---
+++ day2.js
@@ -0,0 +1,27 @@
+console.log("external")
+var a="abhilash";
+console.log(a);
+var num2="31r";
+console.log(num2);
+var underscore_="underscore";
+console.log(underscore_);
+var $="$abhilash";
+console.log($);
+var str="strings are stored in double qutations r single";
+console.log(str);
+var num=33;
+console.log(num);
+var bol=true;
+console.log(bol);
+var a ;
+console.log(a);
+var arr=["abhi" , "bhanu" , "lucky"];
+console.log(arr);
+var object={firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(object);
+
+console.log(typeof ["abhi" ,"bhanu" , "lucky"])
+console.log(typeof 33)   
+console.log(typeof {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}) 
+console.log(typeof true)
+console.log(typeof false)
